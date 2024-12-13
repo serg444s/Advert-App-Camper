394be0cbaa90757a41b5faa0ad5cc6f7ce0c2c42
Serhii Savchenko <sergey.savchenko1303@gmail.com>
2024-12-13 10:39:14 +0200

    upd
    container, home page, nav

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,31 +1,26 @@
-import { NavLink } from "react-router-dom";
-import css from "./Navigation.module.css";
-import clsx from "clsx";
-
+import { NavLink } from 'react-router-dom';
+import css from './Navigation.module.css';
+import clsx from 'clsx';
 
 const Navigation = () => {
- 
   const buildLinkClass = ({ isActive }) => {
     return clsx(css.link, isActive && css.active);
   };
 
-  return ( <>
-
-<nav className={css.nav}>
-      <NavLink to="/" className={buildLinkClass}>
-        Home
-      </NavLink>
-      <NavLink to="/catalog" className={buildLinkClass}>
-      Catalog
-      </NavLink>
-      <NavLink to="/favorites" className={buildLinkClass}>
-      Favorites
-      </NavLink>
-    </nav>
-<div className={css.line}></div>
-
-  </>
-    
+  return (
+    <>
+      <nav className={css.nav}>
+        <NavLink to="/" className={buildLinkClass}>
+          TravelTracks
+        </NavLink>
+        <NavLink to="/catalog" className={buildLinkClass}>
+          Catalog
+        </NavLink>
+        <NavLink to="/favorites" className={buildLinkClass}>
+          Favorites
+        </NavLink>
+      </nav>
+    </>
   );
 };
 
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,23 +1,18 @@
-import { Link } from "react-router-dom";
-import css from "./HomePage.module.css"
-import url from "../../assets/img/camper.jpg"
-
+import { Link } from 'react-router-dom';
+import css from './HomePage.module.css';
 
 const HomePage = () => {
-
-
-    return <>
-
-<h1 className={css.title}>Discover the Freedom of the Open Road with Our Camper Rentals</h1>
-<div className={css.wrapper}>
-    <img src={url} alt="rent camper"/>
-</div>
- <Link to="/catalog" className={css.link}>
-        Select now
+  return (
+    <div className={css.home}>
+      <h1 className={css.title}>Campers of your dreams </h1>
+      <p className={css.text}>
+        You can find everything you want in our catalog
+      </p>
+      <Link to="/catalog" className={css.link}>
+        View Now
       </Link>
-<p className={css.text}>Welcome to our camper rental service, where your adventure begins! Whether you are planning a weekend getaway or an extended road trip, we offer a wide range of well-maintained campers to suit your needs. Our fleet includes everything from compact vans to spacious motorhomes, all equipped with modern amenities to ensure your comfort. Explore breathtaking landscapes, enjoy the convenience of a home on wheels, and create unforgettable memories. Book your perfect camper today and embark on the journey of a lifetime!</p>
-    </>
-}
-
+    </div>
+  );
+};
 
 export default HomePage;
